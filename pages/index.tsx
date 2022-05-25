12108fb01c2f8001f5a6abfd5d3edc3d748579dd
R4d0 <bradek@onet.pl>
2022-05-25 20:20:23 +0200

    PostBox

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="my-7 mx-auto max-w-5xl">
       <Head>
         <title>Redit clone by RB</title>
       </Head>
+      {/* PostBox */}
+      <PostBox />
+      <div className="flex">{/* Feed */}</div>
     </div>
   )
 }
